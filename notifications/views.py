454f56b718fce9d47063b9bdeaddf57318dccce6
Nikki Ng <ng.niccc@gmail.com>
2021-04-29 19:06:00 +0800

    Add CustomerCallbackViewset

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from notifications.models import CustomerCallback
+from notifications.serializers import CustomerCallbackSerializer
+
+
+class CustomerCallbackViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = CustomerCallback.objects.filter(deleted_at__isnull=True)
+    serializer_class = CustomerCallbackSerializer
