abe8258b7171b1a3a0867311ad18aa8ede9bd3d0
Nikki Ng <ng.niccc@gmail.com>
2021-04-30 02:25:32 +0800

    Queue notification job on success

--- notifications/views.py
+++ notifications/views.py
@@ -1,11 +1,12 @@
 from django.db.utils import IntegrityError
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 
 from notifications.models import CustomerCallback
 from notifications.serializers import (CustomerCallbackSerializer,
                                        CustomerMessageSerializer)
+from notifications.tasks import send_notification
 
 
 class CustomerCallbackViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -17,6 +18,11 @@ class CustomerCallbackViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
         self.serializer_class = CustomerMessageSerializer
         request.data["callback"] = pk
         try:
-            return super().create(request)
+            response = super().create(request)
+            if response.status_code == status.HTTP_201_CREATED:
+                send_notification.apply_async(
+                    kwargs={"message_id": response.data["id"]}
+                )
+            return response
         except IntegrityError as e:
             raise ValidationError({"error": str(e)})
