72e3b6e869ec2371a069cf4260a4132f7ca47d62
Nikki Ng <ng.niccc@gmail.com>
2021-04-29 19:18:15 +0800

    Specify read/write only fields

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -1,8 +1,13 @@
-from notifications.models import CustomerCallback
 from rest_framework import serializers
+
+from notifications.models import CustomerCallback
 
 
 class CustomerCallbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomerCallback
-        fields = ["id", "client_secret"]
+        fields = ["id", "callback_url", "client_secret"]
+        read_only_fields = ["id", "client_secret"]
+        extra_kwargs = {
+            "callback_url": {"write_only": True},
+        }
