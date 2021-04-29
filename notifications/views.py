e88086aebbc0bdfca6b7c00c2d89ed117dcb221e
Nikki Ng <ng.niccc@gmail.com>
2021-04-29 21:00:34 +0800

    Add route to handle Notification creation

--- notifications/views.py
+++ notifications/views.py
@@ -1,9 +1,17 @@
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 
 from notifications.models import CustomerCallback
-from notifications.serializers import CustomerCallbackSerializer
+from notifications.serializers import (CustomerCallbackSerializer,
+                                       CustomerMessageSerializer)
 
 
 class CustomerCallbackViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = CustomerCallback.objects.filter(deleted_at__isnull=True)
     serializer_class = CustomerCallbackSerializer
+
+    @action(detail=True, methods=["post"])
+    def notifications(self, request, pk=None):
+        self.serializer_class = CustomerMessageSerializer
+        request.data["callback"] = pk
+        return super().create(request)
