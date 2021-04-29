1b471a09157cb8414512c1039687c42fa96ee713
Nikki Ng <ng.niccc@gmail.com>
2021-04-29 21:00:03 +0800

    Add CustomerMessageSerializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from notifications.models import CustomerCallback
+from notifications.models import CustomerCallback, CustomerMessage
 
 
 class CustomerCallbackSerializer(serializers.ModelSerializer):
@@ -11,3 +11,19 @@ class CustomerCallbackSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "callback_url": {"write_only": True},
         }
+
+
+class CustomerMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerMessage
+        fields = [
+            "id",
+            "external_key",
+            "amount",
+            "account_number",
+            "bank_code",
+            "currency",
+            "transaction_occured_at",
+            "callback",
+        ]
+        read_only_fields = ["id", "transaction_occured_at"]
