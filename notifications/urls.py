e0fd8c609b541fa4c5b91f0efcaf33f71058869e
Nikki Ng <ng.niccc@gmail.com>
2021-04-30 09:00:53 +0800

    Add NotificationViewset

--- notifications/urls.py
+++ notifications/urls.py
@@ -5,5 +5,6 @@ from notifications import views
 app_name = "notifications"
 router = SimpleRouter()
 router.register(r"callbacks", views.CustomerCallbackViewset)
+router.register(r"notifications", views.NotificationViewset)
 
 urlpatterns = router.urls
--- notifications/views.py
+++ notifications/views.py
@@ -2,8 +2,9 @@ from django.db.utils import IntegrityError
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
-from notifications.models import CustomerCallback
+from notifications.models import CustomerCallback, CustomerMessage
 from notifications.serializers import (CustomerCallbackSerializer,
                                        CustomerMessageSerializer)
 from notifications.tasks import send_notification
@@ -26,3 +27,18 @@ class CustomerCallbackViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
             return response
         except IntegrityError as e:
             raise ValidationError({"error": str(e)})
+
+
+class NotificationViewset(viewsets.GenericViewSet):
+    queryset = CustomerMessage.objects.select_related("callback").filter(
+        deleted_at__isnull=True
+    )
+    serializer_class = CustomerMessageSerializer
+
+    @action(detail=True, methods=["post"])
+    def retry(self, request, *args, **kwargs):
+        instance = self.get_object()
+        send_notification.apply_async(
+            kwargs={"message_id": instance.id.hex, "manual_retry": True}
+        )
+        return Response(status=status.HTTP_204_NO_CONTENT)
